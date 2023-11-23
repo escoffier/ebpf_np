3e3100724ee15eabc9656f51fd1691cc46043291
qiuqunfeng <qiuqunfeng@tensorsecurity.cn>
2023-11-23 06:47:29 +0000

    update

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/florianl/go-tc"
 )
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -target bpf  ambient_redirect ./ebpf/net-policy.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -target bpf  microseg_agent ./ebpf/net-policy.c
 
 func attachFilter(attachTo string, program *ebpf.Program) error {
 	return nil
