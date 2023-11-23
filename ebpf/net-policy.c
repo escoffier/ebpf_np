8f99cd6cdd175e61ea3377bad5c8b9f6e33ae8f2
qiuqunfeng <qiuqunfeng@tensorsecurity.cn>
2023-11-23 12:22:49 +0000

    update

--- ebpf/net-policy.c
+++ ebpf/net-policy.c
@@ -38,8 +38,8 @@ struct netpolicy_rule {
 };
 
 struct {
-        __uint(type, BPF_MAP_TYPE_ARRAY);
-        __uint(max_entries, 1);
+        __uint(type, BPF_MAP_TYPE_HASH);
+        __uint(max_entries, 1024);
         __type(key, __u32);
         __type(value, struct netpolicy_rule);
         __uint(pinning, LIBBPF_PIN_BY_NAME);
