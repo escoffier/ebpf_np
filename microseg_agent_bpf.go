c56a4a3886c31401c7e1b1d69500e08ff4bf54d9
qiuqunfeng <qiuqunfeng@tensorsecurity.cn>
2023-11-23 16:21:39 +0800

    update

--- microseg_agent_bpf.go
+++ microseg_agent_bpf.go
@@ -58,7 +58,7 @@ type microseg_agentSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type microseg_agentProgramSpecs struct {
-	TcIngress *ebpf.ProgramSpec `ebpf:"tc_ingress"`
+	TcEgress *ebpf.ProgramSpec `ebpf:"tc_egress"`
 }
 
 // microseg_agentMapSpecs contains maps before they are loaded into the kernel.
@@ -100,12 +100,12 @@ func (m *microseg_agentMaps) Close() error {
 //
 // It can be passed to loadMicroseg_agentObjects or ebpf.CollectionSpec.LoadAndAssign.
 type microseg_agentPrograms struct {
-	TcIngress *ebpf.Program `ebpf:"tc_ingress"`
+	TcEgress *ebpf.Program `ebpf:"tc_egress"`
 }
 
 func (p *microseg_agentPrograms) Close() error {
 	return _Microseg_agentClose(
-		p.TcIngress,
+		p.TcEgress,
 	)
 }
 
