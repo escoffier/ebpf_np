51ddccbf9f185767804c56e5476b28a201d665f0
qiuqunfeng <qiuqunfeng@tensorsecurity.cn>
2023-11-25 02:43:31 +0000

    update

--- ebpf/net-policy.c
+++ ebpf/net-policy.c
@@ -1,58 +1,59 @@
 #include "vmlinux.h"
-// #include <linux/if_ether.h>
-// #include <linux/pkt_cls.h>
-#include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+// #include <linux/bpf.h>
+// #include <linux/if_ether.h>
+// #include <linux/ip.h>
+// #include <linux/pkt_cls.h>
 
 #ifndef __inline
-#define __inline                         \
-   inline __attribute__((always_inline))
+#define __inline inline __attribute__((always_inline))
 #endif
 
-#define ETH_HLEN	14		/* Total octets in header.	 */
+#define ETH_HLEN 14 /* Total octets in header.	 */
 
-#define ETH_P_IP	0x0800		/* Internet Protocol packet	*/
+#define ETH_P_IP 0x0800 /* Internet Protocol packet	*/
 
-
-#define TC_ACT_UNSPEC	(-1)
-#define TC_ACT_OK		0
-#define TC_ACT_RECLASSIFY	1
-#define TC_ACT_SHOT		2
-#define TC_ACT_PIPE		3
-#define TC_ACT_STOLEN		4
-#define TC_ACT_QUEUED		5
-#define TC_ACT_REPEAT		6
-#define TC_ACT_REDIRECT		7
-#define TC_ACT_TRAP		8 /* For hw path, this means "trap to cpu"
-				   * and don't further process the frame
-				   * in hardware. For sw path, this is
-				   * equivalent of TC_ACT_STOLEN - drop
-				   * the skb and act like everything
-				   * is alright.
-				   */
-#define TC_ACT_VALUE_MAX	TC_ACT_TRAP
+#define TC_ACT_UNSPEC (-1)
+#define TC_ACT_OK 0
+#define TC_ACT_RECLASSIFY 1
+#define TC_ACT_SHOT 2
+#define TC_ACT_PIPE 3
+#define TC_ACT_STOLEN 4
+#define TC_ACT_QUEUED 5
+#define TC_ACT_REPEAT 6
+#define TC_ACT_REDIRECT 7
+#define TC_ACT_TRAP                                                            \
+  8 /* For hw path, this means "trap to cpu"                                   \
+     * and don't further process the frame                                     \
+     * in hardware. For sw path, this is                                       \
+     * equivalent of TC_ACT_STOLEN - drop                                      \
+     * the skb and act like everything                                         \
+     * is alright.                                                             \
+     */
+#define TC_ACT_VALUE_MAX TC_ACT_TRAP
 
 struct netpolicy_rule {
-    __u32 from[4];
-    __u32 to[4];
-    __u16 port;
+  __u32 from[4];
+  __u32 to[4];
+  __u16 port;
 };
 
 struct {
-        __uint(type, BPF_MAP_TYPE_HASH);
-        __uint(max_entries, 1024);
-        __type(key, __u32);
-        __type(value, struct netpolicy_rule);
-        __uint(pinning, LIBBPF_PIN_BY_NAME);
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 1024);
+  __type(key, __u32);
+  __type(value, struct netpolicy_rule);
+  __uint(pinning, LIBBPF_PIN_BY_NAME);
 } netpolicy_rule SEC(".maps");
 
 // ipv4 should be in big endian
-static __inline struct netpolicy_rule * get_rule_from_ipv4(__u32 ipv4)
-{
-    return bpf_map_lookup_elem(&netpolicy_rule, &ipv4);
+static __inline struct netpolicy_rule *get_rule_from_ipv4(__u32 ipv4) {
+  return bpf_map_lookup_elem(&netpolicy_rule, &ipv4);
 }
 
-SEC("tc") 
+// for traffics going into container
+SEC("tc")
 int wl_egress(struct __sk_buff *skb) {
   void *data = (void *)(long)skb->data;
   void *data_end = (void *)(long)skb->data_end;
@@ -75,7 +76,7 @@ int wl_egress(struct __sk_buff *skb) {
 
   bpf_printk("tc egress protocol:  %d, source ip: %u, dest ip: %u.\n",
              eth->h_proto, bpf_ntohl(ip->saddr), bpf_ntohl(ip->daddr));
-  
+
   rule = get_rule_from_ipv4(ip->saddr);
   if (rule) {
     bpf_printk("match rule, port %u, drop pkt\n", rule->port);
@@ -83,6 +84,74 @@ int wl_egress(struct __sk_buff *skb) {
   }
 
   return TC_ACT_OK;
+}
+
+// traffic coming from container
+SEC("tc")
+int wl_ingress(struct __sk_buff *skb) {
+  void *data = (void *)(long)skb->data;
+  void *data_end = (void *)(long)skb->data_end;
+  struct netpolicy_rule *rule = NULL;
+
+  if (data_end < data + ETH_HLEN) {
+    return TC_ACT_OK; // Not our packet, return it back to kernel
+  }
+  struct ethhdr *eth = data;
+  if (eth->h_proto != bpf_htons(ETH_P_IP)) {
+    // The protocol is not IPv4, so we can't parse an IPv4 source address.
+    return TC_ACT_OK;
+  }
+
+  // Then parse the IP header.
+  struct iphdr *ip = (void *)(eth + 1);
+  if ((void *)(ip + 1) > data_end) {
+    return TC_ACT_OK;
+  }
+
+  bpf_printk("tc ingress protocol:  %d, source ip: %u, dest ip: %u.\n",
+             eth->h_proto, bpf_ntohl(ip->saddr), bpf_ntohl(ip->daddr));
+
+  rule = get_rule_from_ipv4(ip->saddr);
+  if (rule) {
+    bpf_printk("match rule, port %u, drop pkt\n", rule->port);
+    return TC_ACT_SHOT;
+  }
+
+  return TC_ACT_OK;
+}
+
+SEC("xdp")
+int xdp_ingress(struct xdp_md *ctx) {
+  void *data_end = (void *)(long)ctx->data_end;
+  void *data = (void *)(long)ctx->data;
+
+  struct netpolicy_rule *rule = NULL;
+
+  // First, parse the ethernet header.
+  struct ethhdr *eth = data;
+  if ((void *)(eth + 1) > data_end) {
+    return 0;
+  }
+
+  if (eth->h_proto != bpf_htons(ETH_P_IP)) {
+    // The protocol is not IPv4, so we can't parse an IPv4 source address.
+    return 0;
+  }
+
+  // Then parse the IP header.
+  struct iphdr *ip = (void *)(eth + 1);
+  if ((void *)(ip + 1) > data_end) {
+    return 0;
+  }
+
+  bpf_printk("tc ingress protocol:  %d, source ip: %u, dest ip: %u.\n",
+             eth->h_proto, bpf_ntohl(ip->saddr), bpf_ntohl(ip->daddr));
+
+  rule = get_rule_from_ipv4(ip->saddr);
+  if (rule) {
+    bpf_printk("match rule, port %u, drop pkt\n", rule->port);
+    return TC_ACT_SHOT;
+  }
 }
 
 char __license[] SEC("license") = "GPL";
